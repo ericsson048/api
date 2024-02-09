8c23af61fb9e43a71917dd6e710da349472a7ef9
ericsson048 <eericsson048@email.com>
2024-02-09 11:28:23 +0200

    added permission and search method in django rest framework

--- app/api/views.py
+++ app/api/views.py
@@ -1,8 +1,14 @@
 from app.models import UserProfile
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from app.api.serializer import UserProfileSerializer
+from rest_framework.authentication import TokenAuthentication
+from app.api.permissions import UpdateOwnProfile
 
 
 class UserProfileAV(viewsets.ModelViewSet):
     queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields=('FirstName', 'Email',)
